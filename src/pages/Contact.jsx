8f128cb4997a7bb1da156b72fdb26942cd144a74
YoYic54 <mlymikro@gmail.com>
2023-04-03 10:32:17 +0700

    feat: change font, change link blog, fix: nav

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Contact() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [pesan, setPesan] = useState("");
+  const [check, setCheck] = useState(false);
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    check === true ? setOpen(true) : setOpen(false);
+  });
+  const kirimPesan = (e) => {
+    e.preventDefault();
+    const data = {
+      Email: email,
+      Name: name,
+      Pesan: pesan,
+    };
+    if (open === false) {
+      return alert("Harap Centang Saya Adalah Manusia");
+    }
+    axios
+      .post(
+        "https://sheet.best/api/sheets/f7db2e96-34d7-4c30-8e73-7dbe6f492f69",
+        data
+      )
+      .then((response) => console.log(response));
+    setEmail("");
+    setName("");
+    setPesan("");
+    setCheck(false);
+  };
   return (
     <div className="p-5">
       <h1 className="text-white font-bold text-3xl mb-4 text-center">
         Contact Me
       </h1>
-      <form className="flex flex-col gap-3 items-center mb-20 sm:mb-0">
+      <form
+        className="flex flex-col gap-3 items-center mb-20 sm:mb-0"
+        onSubmit={kirimPesan}
+      >
         <input
           type="email"
           name=""
@@ -14,22 +47,18 @@ function Contact() {
           placeholder="Email"
           className="p-4 rounded-md w-full sm:w-1/2 text-black"
           style={{ boxShadow: "5px 5px 0px #000" }}
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
         />
         <input
           type="text"
           name=""
           id=""
-          placeholder="Username"
+          placeholder="Name"
           className="p-4 rounded-md w-full sm:w-1/2 text-black"
           style={{ boxShadow: "5px 5px 0px #000" }}
-        />
-        <input
-          type="text"
-          name=""
-          id=""
-          placeholder="No Telp"
-          className="p-4 rounded-md w-full sm:w-1/2 text-black"
-          style={{ boxShadow: "5px 5px 0px #000" }}
+          onChange={(e) => setName(e.target.value)}
+          value={name}
         />
         <textarea
           name=""
@@ -37,9 +66,15 @@ function Contact() {
           placeholder="Pesan"
           className="p-4 rounded-md w-full sm:w-1/2 text-black"
           style={{ boxShadow: "5px 5px 0px #000" }}
+          onChange={(e) => setPesan(e.target.value)}
+          value={pesan}
         ></textarea>
         <div className="check flex gap-1">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            onChange={() => setCheck(!check)}
+            checked={check}
+          />
           <p>Saya adalah manusia🤣</p>
         </div>
         <button
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        slab: ["Roboto Slab", "serif"],
+      },
+    },
   },
   plugins: [],
 };
