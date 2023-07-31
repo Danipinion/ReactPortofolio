2db41cbfa0b51908cd6246a672af29f7525df972
YoYic54 <mlymikro@gmail.com>
2023-07-31 19:42:15 +0700

    fixed tugas

--- src/components/profile/Description.jsx
+++ src/components/profile/Description.jsx
@@ -1,84 +1,86 @@
 import React, { useState } from "react";
 function Description() {
-  const [open, setOpen] = useState(false);
-  return (
-    <>
-      <div
-        className=" text-2xl fixed right-10 sm:left-10 sm:top-8 top-5 text-cyan-600"
-        onClick={() => setOpen(!open)}
-        href="#"
-      >
-        {open ? (
-          <ion-icon name="grid"></ion-icon>
-        ) : (
-          <ion-icon name="grid-outline"></ion-icon>
-        )}
-      </div>
-      <div
-        className={`${
-          open
-            ? "sm:inline-block opacity-100 translate-y-0 transition-all duration-300"
-            : "opacity-0 sm:inline-block -translate-y-9 absolute "
-        }`}
-      >
-        <div className="des flex items-center mb-5">
-          <div
-            className="des-img rounded-md p-2 mr-2 bg-cyan-600 text-2xl text-white flex
+	const [open, setOpen] = useState(false);
+	var date = new Date();
+	var born = date.getFullYear() - 2006;
+	return (
+		<>
+			<div
+				className=" text-2xl fixed right-10 sm:left-10 sm:top-8 top-5 text-cyan-600"
+				onClick={() => setOpen(!open)}
+				href="#"
+			>
+				{open ? (
+					<ion-icon name="grid"></ion-icon>
+				) : (
+					<ion-icon name="grid-outline"></ion-icon>
+				)}
+			</div>
+			<div
+				className={`${
+					open
+						? "sm:inline-block opacity-100 translate-y-0 transition-all duration-300"
+						: "opacity-0 sm:inline-block -translate-y-9 absolute "
+				}`}
+			>
+				<div className="des flex items-center mb-5">
+					<div
+						className="des-img rounded-md p-2 mr-2 bg-cyan-600 text-2xl text-white flex
             "
-          >
-            <ion-icon name="phone-portrait-outline"></ion-icon>
-          </div>
-          <div className="des-text text-white text-xs">
-            <div className="title font-light">
-              <p>PHONE</p>
-            </div>
-            <div className="sub-title font-medium">
-              <p>+62895808475151</p>
-            </div>
-          </div>
-        </div>
-        <div className="des flex items-center mb-5">
-          <div className="des-img  rounded-md p-2 mr-2 bg-cyan-600 text-2xl text-white flex">
-            <ion-icon name="mail-outline"></ion-icon>
-          </div>
-          <div className="des-text text-white text-xs">
-            <div className="title font-light">
-              <p>EMAIL</p>
-            </div>
-            <div className="sub-title font-medium">
-              <p>tesdanu@gmail.com</p>
-            </div>
-          </div>
-        </div>
-        <div className="des flex items-center mb-5">
-          <div className="des-img rounded-md p-2 mr-2 bg-cyan-600 text-2xl text-white flex">
-            <ion-icon name="calendar-outline"></ion-icon>
-          </div>
-          <div className="des-text text-white text-xs">
-            <div className="title font-light">
-              <p>AGE</p>
-            </div>
-            <div className="sub-title font-medium">
-              <p>16</p>
-            </div>
-          </div>
-        </div>
-        <div className="des flex items-center mb-5">
-          <div className="des-img rounded-md p-2 mr-2 bg-cyan-600 text-2xl text-white flex stroke-2">
-            <ion-icon name="location-outline"></ion-icon>
-          </div>
-          <div className="des-text text-white text-xs">
-            <div className="title font-light">
-              <p>LOCATION</p>
-            </div>
-            <div className="sub-title font-medium">
-              <p>Madiun, Indonesia</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+					>
+						<ion-icon name="phone-portrait-outline"></ion-icon>
+					</div>
+					<div className="des-text text-white text-xs">
+						<div className="title font-light">
+							<p>PHONE</p>
+						</div>
+						<div className="sub-title font-medium">
+							<p>+62895808475151</p>
+						</div>
+					</div>
+				</div>
+				<div className="des flex items-center mb-5">
+					<div className="des-img  rounded-md p-2 mr-2 bg-cyan-600 text-2xl text-white flex">
+						<ion-icon name="mail-outline"></ion-icon>
+					</div>
+					<div className="des-text text-white text-xs">
+						<div className="title font-light">
+							<p>EMAIL</p>
+						</div>
+						<div className="sub-title font-medium">
+							<p>tesdanu@gmail.com</p>
+						</div>
+					</div>
+				</div>
+				<div className="des flex items-center mb-5">
+					<div className="des-img rounded-md p-2 mr-2 bg-cyan-600 text-2xl text-white flex">
+						<ion-icon name="calendar-outline"></ion-icon>
+					</div>
+					<div className="des-text text-white text-xs">
+						<div className="title font-light">
+							<p>AGE</p>
+						</div>
+						<div className="sub-title font-medium">
+							<p>{born}</p>
+						</div>
+					</div>
+				</div>
+				<div className="des flex items-center mb-5">
+					<div className="des-img rounded-md p-2 mr-2 bg-cyan-600 text-2xl text-white flex stroke-2">
+						<ion-icon name="location-outline"></ion-icon>
+					</div>
+					<div className="des-text text-white text-xs">
+						<div className="title font-light">
+							<p>LOCATION</p>
+						</div>
+						<div className="sub-title font-medium">
+							<p>Madiun, Indonesia</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Description;
--- src/components/profile/Picture.jsx
+++ src/components/profile/Picture.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 function Picture() {
-  return (
-    <div className="flex items-center flex-col">
-      <img
-        src={require("../../assets/img/images.png")}
-        alt="profile"
-        className="rounded-lg w-24"
-      />
-      <h3 className="font-bold text-white my-1 text-xl">Dani</h3>
-      <p className=" badge text-white px-3 py-0.5 text-xs mb-7 w-full text-center bg-cyan-600 rounded-md">
-        Fullstack Engineer
-      </p>
-    </div>
-  );
+	return (
+		<div className="flex items-center flex-col">
+			<img
+				src={require("../../assets/img/profile.JPG")}
+				alt="profile"
+				className="rounded-lg w-24"
+			/>
+			<h3 className="font-bold text-white my-1 text-xl">Dani</h3>
+			<p className=" badge text-white px-3 py-0.5 text-xs mb-7 w-full text-center bg-cyan-600 rounded-md">
+				Fullstack Engineer
+			</p>
+		</div>
+	);
 }
 
 export default Picture;
